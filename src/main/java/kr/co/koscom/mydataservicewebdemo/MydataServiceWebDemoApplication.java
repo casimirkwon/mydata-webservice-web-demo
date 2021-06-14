53c8aacd08c475ed50d65158503785c51b1fb08c
icyz00 <casimir.kwon@gmail.com>
2021-06-14 22:36:51 +0900

    initial import

---
+++ src/main/java/kr/co/koscom/mydataservicewebdemo/MydataServiceWebDemoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.koscom.mydataservicewebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydataServiceWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydataServiceWebDemoApplication.class, args);
+	}
+
+}
