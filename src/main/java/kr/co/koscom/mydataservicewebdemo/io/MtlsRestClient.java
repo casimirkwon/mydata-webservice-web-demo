94e12af01795169da4938be6df3e5ee559d94836
icyz00 <casimir.kwon@gmail.com>
2021-06-16 22:11:34 +0900

    initializing

---
+++ src/main/java/kr/co/koscom/mydataservicewebdemo/io/MtlsRestClient.java
@@ -0,0 +1,44 @@
+package kr.co.koscom.mydataservicewebdemo.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
+public class MtlsRestClient {
+
+	public MtlsRestClient() {
+		
+	}
+	
+	public Object request(String url, Object data) {
+		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+		SSLContext sslContext = null;
+		try {
+			sslContext = SSLContextBuilder.create()
+					.loadKeyMaterial((File)null, null, null)
+					.loadTrustMaterial((File)null, null)
+					.build();
+		} catch (KeyManagementException | UnrecoverableKeyException | NoSuchAlgorithmException | KeyStoreException
+				| CertificateException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		CloseableHttpClient httpClient = HttpClientBuilder.create()
+				.setSSLContext(sslContext)
+				.build();
+
+		return null;
+	}
+}
