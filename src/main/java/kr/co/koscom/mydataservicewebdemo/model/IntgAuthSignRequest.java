93f52fecdf73a959dd282b97a38a18c825c51483
icyz <icyz@koscom.co.kr>
2021-07-02 16:29:02 +0900

    wip

--- src/main/java/kr/co/koscom/mydataservicewebdemo/model/IntgAuthSignRequest.java
+++ src/main/java/kr/co/koscom/mydataservicewebdemo/model/IntgAuthSignRequest.java
@@ -2,6 +2,9 @@ package kr.co.koscom.mydataservicewebdemo.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -38,52 +41,56 @@ public class IntgAuthSignRequest {
 	}
 	
 	@Data
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 	public static class Consent {
 		
-		@ApiModelProperty(value = "정보제공자 기관코드(org_code)", example = "MOC9999999")
+		@ApiModelProperty(value = "정보제공자 기관코드(org_code)", dataType = "string", example = "MOC9999999")
 		private String sndOrgCode;
 		
-		@ApiModelProperty(value = "마이데이터사업자(정보수신자) 기관코드(org_code)", example = "MOC1234567")
+		@ApiModelProperty(value = "마이데이터사업자(정보수신자) 기관코드(org_code)", dataType = "string", example = "MOC1234567")
 		private String rcvOrgCode;
 		
-		@ApiModelProperty(value = "정기적 전송 여부(true 또는 false)", example = "true")
+		@ApiModelProperty(value = "정기적 전송 여부(true 또는 false)", dataType = "string", example = "true")
 		private boolean isScheduled;
 		
 		private Cycle cycle;
 		
-		@ApiModelProperty(value = "전송요구의 종료시점", example = "20221231235959")
+		@ApiModelProperty(value = "전송요구의 종료시점", dataType = "string", example = "20221231235959")
 		private String endDate;
 		
-		@ApiModelProperty(value = "전송을 요구하는 목적", example = "통합계좌 마이데이터 서비스 제공")
+		@ApiModelProperty(value = "전송을 요구하는 목적", dataType = "string", example = "통합계좌 마이데이터 서비스 제공")
 		private String purpose;
 		
-		@ApiModelProperty(value = "전송을 요구하는 개인신용정보의 보유기간", example = "20221231235959")
+		@ApiModelProperty(value = "전송을 요구하는 개인신용정보의 보유기간", dataType = "string", example = "20221231235959")
 		private String holdingPeriod;
 		
 		private List<TargetInfo> targetInfo;
 	}
 	
 	@Data
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 	public static class Cycle {
-		@ApiModelProperty(value = "정기적 전송 주기 단위", example = "1/w")
+		@ApiModelProperty(value = "정기적 전송 주기 단위", dataType = "string", example = "1/w")
 		private String fndCycle;
 		
-		@ApiModelProperty(value = "추가 정보의 정기적 전송 주기 단위", example = "1/d")
+		@ApiModelProperty(value = "추가 정보의 정기적 전송 주기 단위", dataType = "string", example = "1/d")
 		private String addCycle;
 	}
 	
 	
 	@Data
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 	public static class TargetInfo {
-		@ApiModelProperty(value = "전송요구 정보에 해당하는 Scope", example = "invest.list invest.account")
+		@ApiModelProperty(value = "전송요구 정보에 해당하는 Scope", dataType = "string", example = "invest.list")
 		private String scope;
 		
 		private List<Asset> assetList;
 	}
 
 	@Data
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 	public static class Asset {
-		@ApiModelProperty(value = "전송요구 대상 계좌(상품) 식별자", example = "all_asset")
+		@ApiModelProperty(value = "전송요구 대상 계좌(상품) 식별자", dataType = "string", example = "all_asset")
 		private String asset;
 	}
 	
